30a9d004f8f9ca8506d0f50526d2d3b045b2ed82
Miranda <jesusmiranda0304@gmail.com>
2026-03-04 17:51:37 -0600

    [TEST] Models

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -1,5 +1,47 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Reservation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+    it { should belong_to(:room) }
+    it { should belong_to(:user) }
+  end
+
+  describe ".active scope" do
+    let(:room) { Room.create! }
+    let(:user) { User.create!(max_capacity_allowed: 5) }
+
+    let!(:active_reservation) do
+      Reservation.create!(
+        room: room,
+        user: user,
+        starts_at: 1.hour.ago,
+        ends_at: 1.hour.from_now,
+        cancelled_at: nil
+      )
+    end
+
+    let!(:cancelled_reservation) do
+      Reservation.create!(
+        room: room,
+        user: user,
+        starts_at: 1.hour.ago,
+        ends_at: 1.hour.from_now,
+        cancelled_at: Time.current
+      )
+    end
+
+    let!(:past_reservation) do
+      Reservation.create!(
+        room: room,
+        user: user,
+        starts_at: 2.hours.ago,
+        ends_at: 1.hour.ago,
+        cancelled_at: nil
+      )
+    end
+
+    it "returns only non-cancelled and future-ending reservations" do
+      expect(Reservation.active).to contain_exactly(active_reservation)
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+    it { should have_many(:reservations) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:max_capacity_allowed) }
+  end
 end
